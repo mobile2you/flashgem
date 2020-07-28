e3b6ce8fdde41d55d5fb5f5b262ed24bbdd03561
Caio Lopes <caiotuxo@gmail.com>
2020-07-28 09:34:48 -0300

    flash gem

--- lib/m2y_flash/modules/flash_status.rb
+++ lib/m2y_flash/modules/flash_status.rb
@@ -4,7 +4,7 @@ module M2yFlash
 
 		def self.getStatus(client_id, ctt_id)
 			auth = M2yFlash.configuration.api_server_token
-      		url = "#{M2yFlash.configuration.api_server_url}/STATUS_PATH"
+      		url = "#{M2yFlash.configuration.api_server_url}/#{STATUS_PATH}"
       		puts url
       		HTTParty.post(url, 
       			headers: { 'Content-type' => 'application/json',
